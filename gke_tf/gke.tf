42cc1158685edd3ae087616469c17de1fc1adb27
David Vanyan <101475933+DVanyan@users.noreply.github.com>
2023-04-15 20:24:25 +0400

    Update gke.tf

--- gke_tf/gke.tf
+++ gke_tf/gke.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "primary" {
   location                 = var.location
   project                  = var.project_id
   subnetwork               = var.subnetwork
-  initial_node_count       = 2
+  initial_node_count       = 1
   remove_default_node_pool = false
 }
 
