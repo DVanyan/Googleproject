ade8db6f307e846f69adea306d170b8d22f35378
DVanyan <davidvanyan@gmail.com>
2023-04-15 19:51:00 +0400

    tesst

--- gke_tf/gke.tf
+++ gke_tf/gke.tf
@@ -8,7 +8,7 @@ resource "google_container_cluster" "primary" {
   location                 = var.location
   project                  = var.project_id
   subnetwork               = var.subnetwork
-  initial_node_count       = 1
+  initial_node_count       = 2
   remove_default_node_pool = false
 }
 
