4da042759ec0b9aae6cb2819d01e6297fb8c344f
DVanyan <davidvanyan@gmail.com>
2023-04-17 11:57:09 +0400

    experiment2

--- terraform.tfvars
+++
@@ -1,5 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-project_id = "storied-scarab-383615"
-region     = "us-central1"
--- vpc.tf
+++ vpc.tf
@@ -1,38 +1,26 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "project_id" {
-  description = "project id"
-}
-
-variable "region" {
-  description = "region"
-}
-
 resource "github_actions_secret" "GCP_SA_KEY" {
-  repository       = "DVanyan/Googleproject@main"
+  repository       = "git@github.com:DVanyan/Googleproject@main"
   secret_name      = "GCP_SA_KEY"
   plaintext_value  = var.gcp_sa_key
 }
 
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+version = "3.5.0"
+credentials = file("/downloads/instance.json")
+project = "storied-scarab-383615"
+region = "us-central1"
+zone = "us-central1-c"
 }
-
-# VPC
-resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
-  credentials             = var.gcp_sa_key
+resource "google_compute_network" "vpc_network" {
+name = "terraform-network"
 }
-
-# Subnet
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
-  credentials   = var.gcp_sa_key
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "terraform-subnetwork"
+ip_cidr_range = "10.2.0.0/16"
+region = "us-central1"
+network = google_compute_network.vpc_network.name
 }
