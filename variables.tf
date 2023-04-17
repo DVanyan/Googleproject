c056f0a2e686b47b6a88819ec450c5acea3bcdbc
Artur Davtyan <artur.davtyan@webbfontaine.com>
2023-04-17 19:49:16 +0400

    configure

--- variables.tf
+++ variables.tf
@@ -1,5 +1,15 @@
-variable "region" {}
-variable "gcp_project" {}
-variable "credentials" {}
-variable "name" {}
-variable "subnet_cidr" {}
+variable "region" {
+    default = "us-east1"
+}
+variable "gcp_project" {
+    default = "storied-scarab-383615"
+}
+variable "credentials" {
+    default = "credentials.json"
+}
+variable "name" {
+    default = "dev"
+}
+variable "subnet_cidr" {
+    default = "10.10.0.0/24"
+}
