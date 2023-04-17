bbd9a10b26e943d13489ac957d0fc11e2dbae281
DVanyan <davidvanyan@gmail.com>
2023-04-17 16:04:17 +0400

    111

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,7 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
-resource "github_actions_secret" "GCP_SA_KEY" {
-  repository       = "git@github.com:DVanyan/Googleproject@main"
-  secret_name      = "GCP_SA_KEY"
-  plaintext_value  = var.gcp_sa_key
-}
-
-
 provider "google" {
 version = "3.5.0"
-credentials = var.gcp_sa_key
 project = "storied-scarab-383615"
 region = "us-central1"
 zone = "us-central1-c"
