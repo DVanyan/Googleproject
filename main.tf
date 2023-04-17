b24664a7236b1cc000be843baf6578695e9c30cc
Artur Davtyan <artur.davtyan@webbfontaine.com>
2023-04-17 19:56:38 +0400

    add configs

--- main.tf
+++ main.tf
@@ -8,14 +8,20 @@ resource "google_compute_network" "vpc" {
 
 // Create Subnet
 resource "google_compute_subnetwork" "subnet" {
+  depends_on = [
+    google_compute_network.vpc
+  ]
+
   name          = "${var.name}-subnet"
   ip_cidr_range = var.subnet_cidr
   network       = "${var.name}-vpc"
-  depends_on    = ["google_compute_network.vpc"]
   region        = var.region
 }
 // VPC firewall configuration
 resource "google_compute_firewall" "firewall" {
+  depends_on = [
+    google_compute_subnetwork.subnet
+  ]
   name    = "${var.name}-firewall"
   network = google_compute_network.vpc.name
 
