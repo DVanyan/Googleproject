1b3520462fb23879a319f08f96e64bcc8cfbf686
David Vanyan <101475933+DVanyan@users.noreply.github.com>
2023-04-21 16:24:58 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  cloud {
+    organization = "DavidGoogleProject"
+    workspaces {
+      name = "GKE_Create"
+    }
+  }
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+
 // Configure the Google Cloud provider
 
 // Create VPC
